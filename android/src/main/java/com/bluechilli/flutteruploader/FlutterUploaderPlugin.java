c0787f3664b4152cc279dd2793ddf80d1c1af857
Andrews Medina <andrewsmedina@gmail.com>
2025-05-29 09:32:36 -0300

    fix compatibility with flutter 3.29

--- android/src/main/java/com/bluechilli/flutteruploader/FlutterUploaderPlugin.java
+++ android/src/main/java/com/bluechilli/flutteruploader/FlutterUploaderPlugin.java
@@ -15,7 +15,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+// import io.flutter.plugin.common.PluginRegistry.Registrar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -43,15 +43,15 @@ public class FlutterUploaderPlugin implements FlutterPlugin, StatusListener {
       new CachingStreamHandler<>();
   private LiveData<List<WorkInfo>> workInfoLiveData;
 
-  public static void registerWith(Registrar registrar) {
-    final FlutterUploaderPlugin plugin = new FlutterUploaderPlugin();
-    plugin.startListening(registrar.context(), registrar.messenger());
-    registrar.addViewDestroyListener(
-        view -> {
-          plugin.stopListening();
-          return false;
-        });
-  }
+  // public static void registerWith(Registrar registrar) {
+  //   final FlutterUploaderPlugin plugin = new FlutterUploaderPlugin();
+  //   plugin.startListening(registrar.context(), registrar.messenger());
+  //   registrar.addViewDestroyListener(
+  //       view -> {
+  //         plugin.stopListening();
+  //         return false;
+  //       });
+  // }
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
